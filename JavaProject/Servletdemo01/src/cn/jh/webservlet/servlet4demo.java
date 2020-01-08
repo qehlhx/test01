b9217c9590b9647d6ca16ce638a359c2319158e1
admin <954740221@qq.com>
2020-01-08 09:29:30 +0800

    V1.0

---
+++ JavaProject/Servletdemo01/src/cn/jh/webservlet/servlet4demo.java
@@ -0,0 +1,32 @@
+package cn.jh.webservlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+@WebServlet(urlPatterns = "/demo01")
+public class servlet4demo implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("servlet4.0");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
